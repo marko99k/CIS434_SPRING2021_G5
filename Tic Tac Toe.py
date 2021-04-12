2d4b838281173215f22ece8e2a3dad335e3de838
Melanos <aikanaro@live.ru>
2021-04-12 00:06:27 -0400

    Board with X/O without backend.

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,13 +1,13 @@
 from tkinter import *
 
 
-def callback(r, c):
+def callback(l, k):
     global player
     if player == 'X':
-        b[r][c].configure(text='X')
+        b[l][k].configure(text='X')
         player = 'O'
     else:
-        b[r][c].configure(text='O')
+        b[l][k].configure(text='O')
         player = 'X'
 
 
@@ -16,13 +16,14 @@ root = Tk()
 b = [[0, 0, 0],
      [0, 0, 0],
      [0, 0, 0]]
-
+# Initialize the board
 for i in range(3):
     for j in range(3):
         b[i][j] = Button(font=('Verdana', 56), width=3, bg='yellow',
                          command=lambda r=i, c=j: callback(r, c))
         b[i][j].grid(row=i, column=j)
 
+## Assume player 1 is X.
 player = 'X'
 
 mainloop()
