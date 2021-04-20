6b02a024d41edc1248599b01755d1202f354454b
Melanos <aikanaro@live.ru>
2021-04-20 02:25:59 -0400

    Main Menu. Still need to work on the redirection/separate function for the board generation

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,4 +1,48 @@
 from tkinter import *
+from functools import *
+from tkinter import messagebox
+
+
+def play():
+    """Main Menu function for Tic-Tac-Toe in Tkinter"""
+    menu = Tk()
+    menu.geometry("350x350")
+    menu.title("Tic Tac Toe")
+    # aPC = partial(callback, menu) ## Player against PC function needs to be added
+    # aPL = partial(againstPlayer, menu) ## Player against Player function needs to be added
+
+    # Label without functionality
+    head = Label(menu, text="Main Menu", bg="white",
+                 fg="Black", width=500, font=('summer', 12, 'bold'), bd=5)
+
+    B1 = Button(menu, text="Single Player", command=menu.quit,
+                activeforeground='White',
+                activebackground="blue", bg="white",
+                fg="Black", width=500, font='summer', bd=5)
+
+    # command has to be update with the Player Vs Player function
+    B2 = Button(menu, text="Multi Player", command=menu.quit, activeforeground='White',
+                activebackground="blue", bg="white", fg="Black",
+                width=500, font='summer', bd=5)
+
+    # command has to be updated with the againstPC function
+    B3 = Button(menu, text="Exit", command=ExitApplication, activeforeground='White',
+                activebackground="blue", bg="white", fg="Black",
+                width=500, font='summer', bd=5)
+
+    head.pack(side='top')
+    B1.pack(side='top')
+    B2.pack(side='top')
+    B3.pack(side='top')
+    menu.mainloop()
+
+
+def ExitApplication():
+    MsgBox = messagebox.askquestion('Exit Application', 'Are you sure you want to exit the application', icon='warning')
+    if MsgBox == 'yes':
+        quit()
+    else:
+        messagebox.showinfo('Return', 'You will now return to the application screen')
 
 
 def callback(l, k):
@@ -11,12 +55,16 @@ def callback(l, k):
         player = 'X'
 
 
-root = Tk()
-
 b = [[0, 0, 0],
      [0, 0, 0],
      [0, 0, 0]]
+
+root = Tk()
+root.title("Tic Tac Toe")
+
 # Initialize the board
+# global board
+# """Creates global board"""
 for i in range(3):
     for j in range(3):
         b[i][j] = Button(font=('Verdana', 56), width=3, bg='white',
@@ -26,6 +74,5 @@ for i in range(3):
 ## Assume player 1 is X.
 player = 'X'
 
-mainloop()
-
-
+if __name__ == '__main__':
+    play()
